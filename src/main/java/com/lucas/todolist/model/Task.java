732cbde1820a0eed26324a675ee75a77356d06a3
Lucas Rocha <lucasrochadoo123@gmail.com>
2025-06-05 20:41:33 -0300

    criação da entidade task

--- src/main/java/com/lucas/todolist/model/Task.java
+++ src/main/java/com/lucas/todolist/model/Task.java
@@ -1,4 +1,33 @@
 package com.lucas.todolist.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.*;
+
+@Entity
+@Table(name = "Tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Version
+    private Long version;
 }
+
