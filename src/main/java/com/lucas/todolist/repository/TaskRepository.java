270071562bfb1fe9976d6ccbebb5d4f7c1908c6d
Lucas Rocha <lucasrochadoo123@gmail.com>
2025-06-05 20:45:47 -0300

    criação da interface taskRepository para manipulação dos dados no banco de dados

--- src/main/java/com/lucas/todolist/repository/TaskRepository.java
+++ src/main/java/com/lucas/todolist/repository/TaskRepository.java
@@ -1,4 +1,7 @@
 package com.lucas.todolist.repository;
 
-public class TaskRepository {
+import com.lucas.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Long> {
 }
